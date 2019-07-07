04cd4ea4091e6ac02ab4d3976dc78282374897cc
NathanConroy <nathanconroydev@gmail.com>
2019-07-06 22:20:07 -0500

    Updated version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="propargs",
-    version="0.0.7",
+    version="0.0.8",
     author="Nathan Conroy, Gene Callahan",
     author_email="nathanconroydev@gmail.com",
     description="A module for systematically organizing user preferences acquired "
