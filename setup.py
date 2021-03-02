a29a7ec8b1a137c8d4ccfc49c63eefbb4a3df207
NathanConroy <nathanconroydev@gmail.com>
2021-03-02 12:30:19 -0600

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="propargs",
-    version="0.0.15",
+    version="0.0.16",
     author="Nathan Conroy, Gene Callahan",
     author_email="nathanconroydev@gmail.com",
     description="A module for systematically organizing user preferences "
