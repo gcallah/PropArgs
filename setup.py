cf5be53d8f702c9c7a700689551ba5de49b03d4e
Nathan Conroy <nathan.conroy@protonmail.com>
2021-06-02 17:21:28 -0500

    set version = 0.0.17

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="propargs",
-    version="0.0.16",
+    version="0.0.17",
     author="Nathan Conroy, Gene Callahan",
     author_email="nathanconroydev@gmail.com",
     description="A module for systematically organizing user preferences "
